0cf55f5cadc849c755168512d4b482ac4b36e44b
bradfordwagner <wagner.bradford@gmail.com>
2022-10-26 20:27:50 -0400

    setup dummy test

---
+++ pointer_internal_test.go
@@ -0,0 +1,10 @@
+package util
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Pointer", func() {
+	It("tests", func() { Expect(true).To(BeTrue()) })
+})

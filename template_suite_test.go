b52b574e9c0ff3fc05a6d5d902a01878e32030b6
bradfordwagner <wagner.bradford@gmail.com>
2022-10-26 20:26:46 -0400

    rename pkg

--- template_suite_test.go
+++ template_suite_test.go
@@ -1,4 +1,4 @@
-package template
+package util
 
 import (
 	"testing"
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestTemplate(t *testing.T) {
+func TestUtil(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Template Suite")
+	RunSpecs(t, "Util Suite")
 }

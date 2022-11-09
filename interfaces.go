ff8f760fb11ca3244631d513431fb9eb41511c96
Bradford Wagner <wagner.bradford@gmail.com>
2022-11-09 10:20:59 -0500

    Math: Min/Max functions (#18)

--- interfaces.go
+++ interfaces.go
@@ -6,3 +6,8 @@ import "time"
 type Now interface {
 	Now() time.Time
 }
+
+// Number - sets an interface for number types
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64 | uint | uint8 | uint16 | uint32 | uint64
+}

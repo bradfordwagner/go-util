ef293b0133f6ec6d362f13f7b095c211ea454200
bradfordwagner <wagner.bradford@gmail.com>
2024-03-26 16:14:01 -0400

    flag_helper

---
+++ flag_helper/flag_helper_test.go
@@ -0,0 +1,12 @@
+package flag_helper
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("FlagHelper", func() {
+	It("test", func() {
+		Expect(true).To(BeTrue())
+	})
+})

2b3d1b87c4cc61c4cf187bcedec4636c9332ba23
bradfordwagner <wagner.bradford@gmail.com>
2024-03-26 17:00:25 -0400

    improved testing

--- flag_helper/flag_helper_test.go
+++ flag_helper/flag_helper_test.go
@@ -1,12 +1,167 @@
 package flag_helper
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+// TestArgs is a struct that will be used to test the flag_helper
+type TestArgs struct {
+	Hello    string        `mapstructure:"HELLO_WORLD"`
+	Truthy   bool          `mapstructure:"TRUTHY"`
+	Duration time.Duration `mapstructure:"DURATION"`
+}
+
+type args[T any] struct {
+	name         string
+	shortFlag    string
+	defaultValue T
+	description  string
+
+	getField func(a TestArgs) *T
+
+	// when overriding with env var
+	envKey   string
+	envValue string
+}
+type res[T any] struct {
+	expected T
+}
+
+func test[T any](a args[T], r res[T]) {
+	var args TestArgs
+	var cmd = &cobra.Command{}
+	flags := cmd.PersistentFlags()
+	CreateFlag(flags, a.getField(args), a.name, a.shortFlag, a.defaultValue, a.description)
+
+	// override env var
+	if a.envKey != "" {
+		GinkgoT().Setenv(a.envKey, a.envValue)
+	}
+
+	// load env/defaults
+	viper.AutomaticEnv()
+	viper.Unmarshal(&args) // when verbs have divergent args this will need to be moved into cmd specific methods
+
+	var result T = *a.getField(args)
+	Expect(result).To(Equal(r.expected))
+}
+
 var _ = Describe("FlagHelper", func() {
-	It("test", func() {
-		Expect(true).To(BeTrue())
+	It("string,default=default_arg,env=''", func() {
+		test[string](
+			args[string]{
+				name:         "hello_world",
+				shortFlag:    "h",
+				defaultValue: "default_arg",
+				description:  "hello_world, if not provided (env=HELLO_WORLD)",
+				getField:     func(a TestArgs) *string { return &a.Hello },
+				envKey:       "",
+				envValue:     "",
+			},
+			res[string]{
+				expected: "default_arg",
+			},
+		)
+	})
+	It("string,default=env_override,env=env_override", func() {
+		test[string](
+			args[string]{
+				name:         "hello_world",
+				shortFlag:    "h",
+				defaultValue: "default_arg",
+				description:  "hello_world, if not provided (env=HELLO_WORLD)",
+				getField:     func(a TestArgs) *string { return &a.Hello },
+				envKey:       "HELLO_WORLD",
+				envValue:     "env_override",
+			},
+			res[string]{
+				expected: "env_override",
+			},
+		)
+	})
+	It("bool,default=false,env=''", func() {
+		test[bool](
+			args[bool]{
+				name:         "truthy",
+				shortFlag:    "t",
+				defaultValue: false,
+				description:  "truthy, if not provided (env=TRUTHY)",
+				getField:     func(a TestArgs) *bool { return &a.Truthy },
+				envKey:       "",
+				envValue:     "",
+			},
+			res[bool]{
+				expected: false,
+			},
+		)
+	})
+	It("bool,default=true,env=''", func() {
+		test[bool](
+			args[bool]{
+				name:         "truthy",
+				shortFlag:    "t",
+				defaultValue: true,
+				description:  "truthy, if not provided (env=TRUTHY)",
+				getField:     func(a TestArgs) *bool { return &a.Truthy },
+				envKey:       "",
+				envValue:     "",
+			},
+			res[bool]{
+				expected: true,
+			},
+		)
+	})
+	It("bool,default=false,env=true", func() {
+		test[bool](
+			args[bool]{
+				name:         "truthy",
+				shortFlag:    "t",
+				defaultValue: false,
+				description:  "truthy, if not provided (env=TRUTHY)",
+				getField:     func(a TestArgs) *bool { return &a.Truthy },
+				envKey:       "TRUTHY",
+				envValue:     "true",
+			},
+			res[bool]{
+				expected: true,
+			},
+		)
+	})
+	It("duration,default=1s,env=''", func() {
+		test[time.Duration](
+			args[time.Duration]{
+				name:         "duration",
+				shortFlag:    "d",
+				defaultValue: time.Second,
+				description:  "duration, if not provided (env=DURATION)",
+				getField:     func(a TestArgs) *time.Duration { return &a.Duration },
+				envKey:       "",
+				envValue:     "",
+			},
+			res[time.Duration]{
+				expected: time.Second,
+			},
+		)
+	})
+	It("duration,default=1s,env=2s", func() {
+		test[time.Duration](
+			args[time.Duration]{
+				name:         "duration",
+				shortFlag:    "d",
+				defaultValue: time.Second,
+				description:  "duration, if not provided (env=DURATION)",
+				getField:     func(a TestArgs) *time.Duration { return &a.Duration },
+				envKey:       "DURATION",
+				envValue:     "2s",
+			},
+			res[time.Duration]{
+				expected: 2 * time.Second,
+			},
+		)
 	})
 })

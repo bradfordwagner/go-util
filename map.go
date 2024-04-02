a5732781019bed3a756ae6e62049e0569d74fdeb
bradfordwagner <wagner.bradford@gmail.com>
2024-04-02 11:33:41 -0400

    ToSortedSliceByKey

--- map.go
+++ map.go
@@ -1,10 +1,33 @@
 package bwutil
 
+import (
+	"cmp"
+	"slices"
+)
+
 // MapCopy - duplicate a map into a new one
 func MapCopy[k comparable, v any](m map[k]v) (res map[k]v) {
 	res = make(map[k]v)
 	for k, v := range m {
 		res[k] = v
 	}
+	return
+}
+
+// ToSortedSliceByKey - sorts the map by key and returns a slice of sorted values
+func ToSortedSliceByKey[k cmp.Ordered, v any](m map[k]v) (res []v) {
+	keys := make([]k, 0, len(m))
+	for k, _ := range m {
+		keys = append(keys, k)
+	}
+
+	slices.Sort(keys)
+
+	// use sorted keys to get values in order
+	res = make([]v, 0, len(m))
+	for _, k := range keys {
+		res = append(res, m[k])
+	}
+
 	return
 }

dc1cc13ee0480e0083b9c64a680505d53211e4ac
Bradford Wagner <wagner.bradford@gmail.com>
2024-04-02 11:34:31 -0400

    ToSortedSliceByKey (#31)

--- map_test.go
+++ map_test.go
@@ -1,14 +1,14 @@
 package bwutil_test
 
 import (
-	"github.com/bradfordwagner/go-util"
+	"reflect"
+
+	bwutil "github.com/bradfordwagner/go-util"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"reflect"
 )
 
 var _ = Describe("Map", func() {
-	It("test", func() { Expect(true).To(BeTrue()) })
 	Context("MapCopy", func() {
 		It("passes deep equals", func() {
 			orig := map[int]int{
@@ -23,6 +23,26 @@ var _ = Describe("Map", func() {
 			Expect(reflect.DeepEqual(orig, cp)).To(BeFalse())
 			_, exists := orig[3]
 			Expect(exists).To(BeFalse())
+		})
+	})
+
+	Context("ToSortedSliceByKey", func() {
+		It("sorts by key", func() {
+			m := map[int]int{
+				5: 5,
+				4: 4,
+				3: 3,
+				2: 2,
+				1: 1,
+				0: 0,
+			}
+			s := bwutil.ToSortedSliceByKey(m)
+			Expect(s[0]).To(Equal(0))
+			Expect(s[1]).To(Equal(1))
+			Expect(s[2]).To(Equal(2))
+			Expect(s[3]).To(Equal(3))
+			Expect(s[4]).To(Equal(4))
+			Expect(s[5]).To(Equal(5))
 		})
 	})
 })

4b63206feeb527bac97ae7442400e2f59c1e178b
Bradford Wagner <wagner.bradford@gmail.com>
2024-06-20 16:22:07 -0400

    feat(log): level (#32)
    * feat(log): level
    
    * update
    
    * fix naming colision
    
    * force init

--- log/log.go
+++ log/log.go
@@ -7,15 +7,29 @@ import (
 
 var logger *zap.SugaredLogger
 
-func init() {
+var DefaultConfig = Config{
+	Level: "info",
+}
+
+// Config is the configuration for the logger.
+type Config struct {
+	Level string
+}
+
+// Init initializes the logger with the given configuration.
+func Init(conf Config) {
 	encoderCfg := zap.NewProductionEncoderConfig()
 	encoderCfg.TimeKey = "timestamp"
 	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	level, err := zapcore.ParseLevel(conf.Level)
+	if err != nil {
+		level = zapcore.InfoLevel
+	}
 	c := zap.Config{
 		EncoderConfig:    encoderCfg,
 		Encoding:         "console",
 		ErrorOutputPaths: []string{"stderr"},
-		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		Level:            zap.NewAtomicLevelAt(level),
 		OutputPaths:      []string{"stderr"},
 	}
 	l, _ := c.Build()
@@ -23,5 +37,8 @@ func init() {
 }
 
 func Log() *zap.SugaredLogger {
+	if logger == nil {
+		Init(DefaultConfig)
+	}
 	return logger
 }

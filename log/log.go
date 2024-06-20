bf72bac499029d828639db9aff6b4a8ce84930d0
bradfordwagner <wagner.bradford@gmail.com>
2024-06-20 14:19:23 -0400

    feat(log): level

--- log/log.go
+++ log/log.go
@@ -7,6 +7,28 @@ import (
 
 var logger *zap.SugaredLogger
 
+type Level string
+
+const (
+	DebugLevel  Level = "debug"
+	InfoLevel   Level = "info"
+	WarnLevel   Level = "warn"
+	ErrorLevel  Level = "error"
+	DPanicLevel Level = "dpanic"
+	PanicLevel  Level = "panic"
+	FatalLevel  Level = "fatal"
+)
+
+var levelConversion = map[Level]zapcore.Level{
+	DebugLevel:  zap.DebugLevel,
+	InfoLevel:   zap.InfoLevel,
+	WarnLevel:   zap.WarnLevel,
+	ErrorLevel:  zap.ErrorLevel,
+	DPanicLevel: zap.DPanicLevel,
+	PanicLevel:  zap.PanicLevel,
+	FatalLevel:  zap.FatalLevel,
+}
+
 func init() {
 	encoderCfg := zap.NewProductionEncoderConfig()
 	encoderCfg.TimeKey = "timestamp"
@@ -15,12 +37,14 @@ func init() {
 		EncoderConfig:    encoderCfg,
 		Encoding:         "console",
 		ErrorOutputPaths: []string{"stderr"},
-		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		Level:            zap.NewAtomicLevelAt(levelConversion[LevelConfig]),
 		OutputPaths:      []string{"stderr"},
 	}
 	l, _ := c.Build()
 	logger = l.Sugar()
 }
+
+var LevelConfig Level = InfoLevel
 
 func Log() *zap.SugaredLogger {
 	return logger

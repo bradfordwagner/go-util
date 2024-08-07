1772c160a32b1831805a18569a5a027f9bbb4b53
bradfordwagner <wagner.bradford@gmail.com>
2024-08-07 18:04:32 -0400

    move to root

--- bwutil/mocks.go
+++
@@ -1,3 +0,0 @@
-package bwutil
-
-//go:generate mockgen -destination=mocks/bwutil/module.go -package=mock_bwutil github.com/bradfordwagner/go-util StubOneOf,StubConversionOneOf
---
+++ mocks.go
@@ -0,0 +1,3 @@
+package bwutil
+
+//go:generate mockgen -destination=mocks/bwutil/module.go -package=mock_bwutil github.com/bradfordwagner/go-util/bwutil StubOneOf,StubConversionOneOf
